f00b6bc59cef1f64fc6ea8cd610d4a9619a65ee9
Blizzorde <119112639+Blizzorde@users.noreply.github.com>
2026-05-15 20:48:56 -0300

    fix: typo in hashed var

--- litterly-backend/routes/authRoutes.js
+++ litterly-backend/routes/authRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/register", async (req, res) => {
   const { email, username, password } = req.body;
   try {
-    const hashed_real = await bcrypt.hash(password, 10);
+    const hashed = await bcrypt.hash(password, 10);
 
     await pool.query(
       "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)",
