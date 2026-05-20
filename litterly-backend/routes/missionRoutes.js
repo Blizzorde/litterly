3d4cc7854291d02cc338d7bad0742e3cd8a70d1b
ANKA <alimoenadinka@gmail.com>
2026-05-20 19:47:59 -0300

    slight change to request

--- litterly-backend/routes/missionRoutes.js
+++ litterly-backend/routes/missionRoutes.js
@@ -18,16 +18,17 @@ router.get("/", async (req, res) => {
 router.post("/join", authMiddleware, async (req, res) => {
     const { missionId } = req.body;
     const userId = req.session.user.id;
+    
 
     try {
         await pool.query(
-            "INSERT INTO mission_participants (user_id, mission_id) VALUES (?, ?)",
-            [userId, missionId]
+            "INSERT INTO mission_participants (user_id, mission_id, status, registered_at) VALUES (?, ?, ?, ?)",
+            [missionId, userId, status, registered_at]
         );
 
         res.json({ message: "Joined mission" });
     } catch (err) {
-        res.status(500).json({ message: "Error joining mission" });
+        res.status(500).json({ message: "Error joining mission", err: err.message});
     }
 });
 
