a5e03c1ec389e1ee02cc1550b916420463efde60
Blizzorde <119112639+Blizzorde@users.noreply.github.com>
2026-05-24 13:18:24 -0300

    Added Conditional Displaying Logic

--- litterly-frontend/js/main.js
+++ litterly-frontend/js/main.js
@@ -22,6 +22,42 @@ async function authGuard() {
 
   // Store user in memory for this page to use
   window.currentUser = user;
+  populateNavbar(user);
+  applyRoleVisibility(user);
 }
+
+function populateNavbar(user) {
+  if (!user) return;
+
+  const nameEl = document.querySelector("#username");
+  const pointsEl = document.querySelector("#points");
+
+  if (nameEl) nameEl.textContent = user.username;
+  if (pointsEl) pointsEl.textContent = user.points;
+}
+
+function hasRole(...roles) {
+  return window.currentUser && roles.includes(window.currentUser.role);
+}
+
+function applyRoleVisibility(user) {
+  if (!user) return;
+  document.querySelectorAll("[data-role]").forEach((el) => {
+    const roles = el.dataset.role.split(",");
+    console.log(roles);
+    console.log(user);
+    el.style.display = roles.includes(user.role_id.toString())
+      ? "block"
+      : "none";
+  });
+}
+
+document
+  .querySelector(".logout-section a")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    await logoutUser();
+    window.location.href = "/pages/login.html";
+  });
 
 authGuard();
